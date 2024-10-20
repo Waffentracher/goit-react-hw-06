9efd7554813d355123913b5c53236aaf08672e76
Waffentracher <142826622+Waffentracher@users.noreply.github.com>
2024-10-20 19:09:47 +0300

    Update contactsSlice.js
    add save contacts after reload

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,18 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  items: JSON.parse(localStorage.getItem("contacts")) || [],
+};
+
 const contactsSlice = createSlice({
   name: "contacts",
-  initialState: {
-    items: [],
-  },
+  initialState,
   reducers: {
     addContact: (state, action) => {
       state.items.push(action.payload);
+      localStorage.setItem("contacts", JSON.stringify(state.items));
     },
     deleteContact: (state, action) => {
       state.items = state.items.filter(
         (contact) => contact.id !== action.payload
       );
+      localStorage.setItem("contacts", JSON.stringify(state.items));
     },
   },
 });
