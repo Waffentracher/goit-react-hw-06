e7bae9c44a8e5d59ea4e00ea7fc8e72ed1e8612d
Waffentracher <142826622+Waffentracher@users.noreply.github.com>
2024-11-04 14:00:27 +0200

    delete local in store

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-// src/App.jsx
-
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './redux/store';
+import { store } from './redux/store';
 import ContactsForm from './components/ContactForm/ContactForm';
 import ContactList from './components/ContactList/ContactList';
 import SearchBox from './components/SearchBox/SearchBox';
@@ -10,14 +7,12 @@ import './App.css';
 
 const App = () => (
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <div className="App">
-        <h1>Contact Book</h1>
-        <ContactsForm />
-        <SearchBox />
-        <ContactList />
-      </div>
-    </PersistGate>
+    <div className="App">
+      <h1>Contact Book</h1>
+      <ContactsForm />
+      <SearchBox />
+      <ContactList />
+    </div>
   </Provider>
 );
 
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,28 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  items: JSON.parse(localStorage.getItem("contacts")) || [],
+  items: [],
 };
 
 const contactsSlice = createSlice({
   name: "contacts",
   initialState,
   reducers: {
+    setContacts: (state, action) => {
+      state.items = action.payload;
+    },
     addContact: (state, action) => {
       state.items.push(action.payload);
-      localStorage.setItem("contacts", JSON.stringify(state.items));
     },
     deleteContact: (state, action) => {
       state.items = state.items.filter(
         (contact) => contact.id !== action.payload
       );
-      localStorage.setItem("contacts", JSON.stringify(state.items));
     },
   },
 });
 
-export const { addContact, deleteContact } = contactsSlice.actions;
-
+export const { setContacts, addContact, deleteContact } = contactsSlice.actions;
 export const selectContacts = (state) => state.contacts.items;
 
 export default contactsSlice.reducer;
